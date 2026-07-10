2a8ddb43af99ceaf18fd295c50a54a4ae6d0d748
Mahfuz <mahfuzru5634@gmail.com>
2026-07-10 23:51:41 +0600

    feat: add validation schemas for user registration and login; implement request validation middleware

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,10 +5,13 @@ dotenv.config({path:path.join(process.cwd(),".env")});
 
 export default{
     port:parseInt(process.env.PORT || "5000"),
+    jwt_access_secret:process.env.JWT_ACCESS_SECRET,
+    jwt_expires_in:process.env.JWT_ACCESS_EXPIRES_IN
+
     // app_url:process.env.APP_URL || "http://localhost:5000",
     // // database_url:process.env.DATABASE_URL,
     // // bycrypt_salt_rounds:process.env.BCRYPT_SALT_ROUNDS || 10,
-    // // jwt_access_secret:process.env.JWT_ACCESS_SECRET,
+   
     // // jwt_refresh_secret:process.env.JWT_REFRESH_SECRET,
     // // jwt_access_expiration_time:process.env.JWT_ACCESS_EXPIRATION_TIME || "15m", 
     // // jwt_refresh_expiration_time:process.env.JWT_REFRESH_EXPIRATION_TIME || "7d",
